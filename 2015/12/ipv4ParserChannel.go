86642afa107eeb4a29faae4cfa2096ee4d9b2103
Ray Imber <ray.imber@galavantier.com>
2015-12-02 15:08:26 -0800

    Last minute updates to the parser combinator talk

--- 2015/12/ipv4ParserChannel.go
+++ 2015/12/ipv4ParserChannel.go
@@ -18,6 +18,7 @@ func parseChar(search rune, terminal string, outputChan chan parseNode) Parser {
 		r, _, err := input.RuneReader.ReadRune()
 		if err != nil {
 			input.UnreadRune()
+			outputChan <- err
 			return false
 		}
 		if r == search {
@@ -32,6 +33,9 @@ func parseChar(search rune, terminal string, outputChan chan parseNode) Parser {
 
 func parseNum(input io.RuneScanner) bool {
 	r, _, err := input.RuneReader.ReadRune()
+	if err != nil {
+		outputChan <- err
+	}
 	if r < 48 || r > 57 {
 		input.UnreadRune()
 		return false
@@ -73,6 +77,8 @@ func ipv4Listener(outputChan chan parseNode) (string, error) {
 		switch t := node.(type) {
 		default:
 			results = append(results, node)
+		case error:
+			return "", t
 		case rune:
 			// If it's a rune, convert it to a string
 			results = append(results, fmt.Sprintf("%c", node))
@@ -87,7 +93,7 @@ func ipv4Listener(outputChan chan parseNode) (string, error) {
 					return nil, err
 				}
 				if number > 255 {
-					return nil, errors.New(fmt.Sprint("Invalid Subnet:", number))
+					return "", errors.New(fmt.Sprint("Invalid Subnet:", number))
 				}
 				// Push the subnet string back onto the stack
 				results = append(results, subnetStr)
